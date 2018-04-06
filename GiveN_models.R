6d4d03d2d6ec4295774079600d5fcf0bbb1d97b1
Emily Carrigan <33790869+emlini@users.noreply.github.com>
2018-04-06 16:15:19 -0400

    Add files via upload

---
+++ GiveN_models.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(ggplot2)
+
+#Check and then set working dir
+#getwd()
+#setwd("[filepath]")
+
+GiveN <- read.csv("GiveN_Coding_180405_AB.csv") #import the data file (which I already saved as a csv)
+View(GiveN)
+
+#data cleaning
+GiveN_subset <- subset(GiveN, GiveN$Important.Notes=='')
+View(GiveN_subset)
+GiveN_subset <- GiveN_subset[1:101,1:17] #there were blank rows in the dataframe, and columns I didn't need
+
+#visualize the data (task performance--Knower level by age, across different language groups)
+Age <- GiveN_subset$Age
+KL <- GiveN_subset$Knower.level_GiveN_Small
+
+#reorder factor levels for Language Group so they show up in order I want them
+GiveN_subset$Group_4cat <- factor(as.character(GiveN_subset$Group_4cat), levels = c("English Early", "ASL Early", "English Later", "ASL Later"))
+
+
+p <- ggplot(GiveN_subset, aes(x=Age, y=KL)) + geom_point() + geom_smooth(method=lm)
+p + facet_wrap(~GiveN_subset$Group_4cat) #facet_wrap doesn't work with assigned variables
+#so if I do "LangGrp <- GiveN_subset$Group_4cat" I can use LangGrp in a model, but not in facet_wrap
+
+cor.test(Age, KL)
+cor.test(Age, KL)
