86e52e053d56438a31dd180920683c2dafe65917
Emily Carrigan <33790869+emlini@users.noreply.github.com>
2018-04-24 19:52:48 -0400

    Update GiveN_models.R

--- GiveN_models.R
+++ GiveN_models.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(ggplot2)
+library(beeswarm)
+library("colorspace")
 
 #Check and then set working dir
 #getwd()
-#setwd("[filepath]")
+setwd("/Volumes/YARN STASH/Dropbox/SLAM - Number Project/SLAM Analysis/GiveN_AB")
 
 GiveN <- read.csv("GiveN_Coding_180405_AB.csv") #import the data file (which I already saved as a csv)
 View(GiveN)
@@ -28,12 +30,74 @@ p + facet_wrap(~GiveN_subset$Group_4cat) #facet_wrap doesn't work with assigned 
 cor.test(Age, KL)
 cor.test(Age, KL)
 
+
 #BEESWARM PLOTS!!!  Spectacular data visualization for GiveN data by Age
 
-library(beeswarm)
-beeswarm(Age~KL, data= GiveN_subset, subset= GiveN_subset$Group_4cat=='English Early', pch=16, col=rainbow((7)), xlab = "Give-N Small Knower Level", ylab= "Age", main="Knower Level by Age: 'English Early' Group")
-beeswarm(Age~KL, data= GiveN_subset, subset= GiveN_subset$Group_4cat=='English Later', pch=16, col=rainbow((7)), xlab = "Give-N Small Knower Level", ylab= "Age", main="Knower Level by Age: 'English Later' Group")
-beeswarm(Age~KL, data= GiveN_subset, subset= GiveN_subset$Group_4cat=='ASL Early', pch=16, col=rainbow((7)), xlab = "Give-N Small Knower Level", ylab= "Age", main="Knower Level by Age: 'ASL Early' Group")
-beeswarm(Age~KL, data= GiveN_subset, subset= GiveN_subset$Group_4cat=='ASL Later', pch=16, col=rainbow((7)), xlab = "Give-N Small Knower Level", ylab= "Age", main="Knower Level by Age: 'ASL Later' Group")
+#KL is an integer.  To use it in beeswarm plot (and have all possible values show up on all graphs--even values for which a particular group has no data points at that value--I have to turn it into a factor)
+KL_factor <- as.factor(KL)
 
-# NEXT THING TO CHANGE HERE IS COLORS APPLIED TO SPECIFIC KNOWER-LEVELS (so 0 is always red, for instance)
+#add newly created, factorized KL variable to the dataset
+GiveN_subset <- cbind(GiveN_subset, KL_factor)
+View(GiveN_subset)
+
+#re-assign variables now that I've changed the dataframe
+Age <- GiveN_subset$Age
+KL_factor <- GiveN_subset$KL_factor
+
+#To get colors to match up to levels of Give-N variable, create variable for color (makes easier to insert into beeswarm code)
+#(from https://www.nceas.ucsb.edu/~frazier/RSpatialGuides/colorPaletteCheatsheet.pdf)
+
+GiveN_col <- factor(KL_factor, levels=c(0, 1, 2, 3, 4, 5, 6), labels=c("red1", "orange1", "yellow2", "green1", "blue1", "plum1", "purple1"))
+
+
+#allowing automatic x-axis labeling WITH FACTOR!!
+#If try to coerce the x-axis here with axis() it messes things up
+
+beeswarm(Age~KL_factor,
+	data= GiveN_subset,
+	subset= GiveN_subset$Group_4cat=='English Early',
+	pch=16,
+	pwcol=as.character(GiveN_col),
+	ylim=c(3,8),
+	yaxp = c(3, 8, 5),
+	xlab = "Give-N Small Knower Level",
+	ylab= "Age (years)",
+	main="Knower Level by Age: 'English Early' Group")
+
+beeswarm(Age~KL_factor,
+	data= GiveN_subset,
+	subset= GiveN_subset$Group_4cat=='English Later',
+	pch=16,
+	pwcol=as.character(GiveN_col),
+	ylim=c(3,8),
+	xaxp = c(0, 6, 6),
+	yaxp = c(3, 8, 5), 
+	xlab = "Give-N Small Knower Level", 
+	ylab= "Age (years)", 
+	main="Knower Level by Age: 'English Later' Group")
+
+
+beeswarm(Age~KL_factor, 
+	data= GiveN_subset, 
+	subset= GiveN_subset$Group_4cat=='ASL Early', 
+	method = "center", 
+	pch=16, 
+	pwcol=as.character(GiveN_col),
+	ylim=c(3,8),  
+	yaxp = c(3, 8, 5), 
+	xlab = "Give-N Small Knower Level", 
+	ylab= "Age (years)", 
+	main="Knower Level by Age: 'ASL Early' Group")
+
+
+beeswarm(Age~KL_factor, 
+	data= GiveN_subset, 
+	subset= GiveN_subset$Group_4cat=='ASL Later', 
+	method = "center", 
+	pch=16, 
+	pwcol=as.character(GiveN_col),  
+	ylim=c(3,8), 
+	yaxp = c(3, 8, 5), 
+	xlab = "Give-N Small Knower Level", 
+	ylab= "Age (years)", 
+	main="Knower Level by Age: 'ASL Later' Group")
